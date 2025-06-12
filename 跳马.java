cae4deba4aad61704013ccc939afe2dbd0894fb3
xxlxxl123123 <3081245810@qq.com>
2025-06-12 22:44:43 +0800

    Update 跳马.java

--- 跳马.java
+++ 跳马.java
@@ -1,51 +1,81 @@
-N = 8  # 棋盘大小
-M = 9
-x = 0  # 坐标
-y = 0
-D = 0  # 方向
-count = 0  # 走法数
-s = []  # 栈
+import java.util.*;
 
-def push():
-    s.append({'x': x, 'y': y, 'D': D})
+public class Tm {
+    //棋盘大小
+    private static int N=8;
+    private static int M=9;
+    //坐标
+    private static int x=0;
+    private static int y=0;
+    //方向
+    private static int D=0;
+    //走法数
+    private static int count = 0;
+    //栈
+    private static List<Map<String,Integer>> s = new ArrayList<>();
+    public static void main(String[] args) {
+        while (s.size()>0||D<4){
+            if(D<4){
+                push();
+                zou();
+                if(x>N||y>M||x<0||y<0){
+                    pop();
+                    D++;
+                }else{
+                    if(x==N&&y==M){
+                        count++;
+                        System.out.println(s);
+                    }else{
+                        D=0;
+                    }
+                }
+            }else {
+                pop();
+                D++;
+            }
+        }
+        System.out.println(count);
 
-def pop():
-    global x, y, D
-    last = s.pop()
-    x = last['x']
-    y = last['y']
-    D = last['D']
+    }
 
-def zou():
-    global x, y
-    if D == 0:
-        x += 1
-        y += 2
-    elif D == 1:
-        x += 2
-        y += 1
-    elif D == 2:
-        x += 2
-        y -= 1
-    elif D == 3:
-        x += 1
-        y -= 2
+    public static void pop() {
+        Map<String,Integer> map=new HashMap<String,Integer>();
+        map = s.remove(s.size() - 1);
+        x=map.get("x");
+        y=map.get("y");
+        D=map.get("D");
+    }
 
-while len(s) > 0 or D < 4:
-    if D < 4:
-        push()
-        zou()
-        if x > N or y > M or x < 0 or y < 0:
-            pop()
-            D += 1
-        else:
-            if x == N and y == M:
-                count += 1
-                print(s)
-            else:
-                D = 0
-    else:
-        pop()
-        D += 1
+    public static void push(){
+        Map<String,Integer> map=new HashMap<String,Integer>();
+        map.put("x",x);
+        map.put("y",y);
+        map.put("D",D);
+        s.add(map);
+    }
 
-print(count)
+    public static void zou (){
+        switch (D){
+            case 0:{
+                x+=1;
+                y+=2;
+                break;
+            }
+            case 1:{
+                x+=2;
+                y+=1;
+                break;
+            }
+            case 2:{
+                x+=2;
+                y-=1;
+                break;
+            }
+            case 3:{
+                x+=1;
+                y-=2;
+                break;
+            }
+        }
+    }
+}
